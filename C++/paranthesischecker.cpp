5eacf3c78c659a2be17370ef61b891879db5e3f1
Achalesh Lakhotiya <2019kuec2036@iiitkota.ac.in>
2022-11-23 15:39:06 +0530

    Create paranthesischecker.cpp

---
+++ C++/paranthesischecker.cpp
@@ -0,0 +1,64 @@
+// Tackle the empty string ko valid string print kraane vala scene.
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isValid(string s)
+{
+    stack<char> stk;
+    char x;
+    for(int i = 0; i < s.length(); i++)
+    {
+        if(s[i] == '('|| s[i] == '[' || s[i] =='{')
+        {
+            stk.push(s[i]);
+            continue;
+        }
+        if(stk.empty())
+        {
+            return false;
+        }
+       
+        switch(s[i])
+        {
+            case ')':
+                x = stk.top();
+                stk.pop();
+                if(x == '{' || x == '[')
+                {
+                    return false;
+                }
+                break;
+            case ']':
+                x = stk.top();
+                stk.pop();
+                if(x == '(' || x == '{')
+                {
+                    return false;
+                }
+                break;
+            case '}':
+                x = stk.top();
+                stk.pop();
+                if(x == '(' || x == '[')
+                {
+                    return false;
+                }
+                break;     
+        }
+        
+    }
+    return (stk.empty());
+}
+int main()
+{
+    string s = "";
+    if(isValid(s))
+    {
+        cout<<"Valid String";
+    }
+    else
+    {
+        cout<<"Invalid String";
+    }
+    return 0;
+}
